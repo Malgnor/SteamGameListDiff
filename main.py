60d288d9ca9eb0fe77a20af50022d887a558cba3
Alex <aamlima123@gmail.com>
2017-12-22 21:09:43 -0200

    SteamID

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 from flask import Flask
+from steam_api import get_steamid
 app = Flask(__name__)
 
 
@@ -8,6 +8,26 @@ def hello_world():
     return 'Hello, World!'
 
 
-# @app.route('/key')
-# def show_key():
-#     return 'Steam web api key: ' + os.getenv('STEAM_WEB_API_KEY', 'NO KEY :(')
+@app.route('/<user1>/<user2>/')
+def compare_users(user1, user2):
+    errors = []
+    try:
+        userid1 = int(user1)
+    except ValueError:
+        try:
+            userid1 = get_steamid(user1)
+        except ValueError as exception:
+            errors.append(str(exception))
+
+    try:
+        userid2 = int(user2)
+    except ValueError:
+        try:
+            userid2 = get_steamid(user2)
+        except ValueError as exception:
+            errors.append(str(exception))
+
+    if len(errors) is not 0:
+        return '<br>'.join(errors)
+
+    return 'User1: ' + str(userid1) + '<br>User2: ' + str(userid2)
---
+++ steam_api.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+STEAM_WEB_API_KEY = os.getenv('STEAM_WEB_API_KEY', 'NO KEY :(')
+
+
+def get_steamid(vanity_name):
+    req = requests.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/',
+                       {'key': STEAM_WEB_API_KEY, 'vanityurl': vanity_name})
+    if req.status_code == 200:
+        res = req.json()['response']
+        if res['success'] == 1:
+            return res['steamid']
+        raise ValueError('Vanity name {} not found.'.format(vanity_name))
+    raise ValueError('ResolveVanityURL status response: ' + res.status_code)
+
+
+# http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=XXXXXXXXXXXXXXXXXXXXXXX&vanityurl=userVanityUrlName
+# http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=XXXXXXXXXXXXX&include_appinfo=true&format=json
+# http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{hash}.jpg

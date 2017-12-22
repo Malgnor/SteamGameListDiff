d08761f79b8e3a3aa25d419f8b20d53a207d913e
Alex <aamlima123@gmail.com>
2017-12-22 18:36:05 -0200

    Key?

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/key')
+def show_key():
+    return 'Steam web api key: ' + os.getenv('STEAM_WEB_API_KEY', 'NO KEY :(')

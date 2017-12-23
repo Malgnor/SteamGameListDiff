0df35d68fe2cc81cbfceb39e49b1e3c1fa3f18f5
Alex <aamlima123@gmail.com>
2017-12-23 00:46:47 -0200

    Added compare template

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from steam_api import get_steamid, get_owned_games
+from flask import Flask, render_template
+from steam_api import get_steamid, get_owned_games, get_player_summaries
 app = Flask(__name__)
 
 
@@ -31,6 +31,14 @@ def compare_users(user1, user2):
         return '<br>'.join(errors)
 
     try:
+        profile1, profile2 = get_player_summaries(userid1, userid2)
+    except ValueError as exception:
+        errors.append(str(exception))
+
+    if len(errors) is not 0:
+        return '<br>'.join(errors)
+
+    try:
         games1 = get_owned_games(userid1)
     except ValueError as exception:
         errors.append(str(exception))
@@ -43,25 +51,21 @@ def compare_users(user1, user2):
     if len(errors) is not 0:
         return '<br>'.join(errors)
 
-    list1 = {v['appid']: v['name'] for v in games1}
-    list2 = {v['appid']: v['name'] for v in games2}
+    list1 = {v['appid']: v for v in games1}
+    list2 = {v['appid']: v for v in games2}
 
     list3 = {}
     list4 = {}
     list5 = {}
 
-    for appid, name in list1.items():
+    for appid, game in list1.items():
         if appid in list2:
-            list3.update({appid: name})
+            list3.update({appid: game})
         else:
-            list4.update({appid: name})
+            list4.update({appid: game})
 
-    for appid, name in list2.items():
+    for appid, game in list2.items():
         if appid not in list3:
-            list5.update({appid: name})
+            list5.update({appid: game})
 
-    print(str(len(list1) + len(list2)),
-          str(len(list3) + len(list4) + len(list5)))
-
-    return 'User1+User2: ' + str(list3.values()) + '<br><br>User1: ' + str(list4.values()) + '<br><br>User2: ' + str(list5.values())
-    # return 'User1: ' + str(userid1) + '<br>User2: ' + str(userid2) + '<br>Games1: ' + str(games1) + '<br>Games2: ' + str(games2)
+    return render_template('compare.html', games_user1=list4.values(), games_user2=list5.values(), games_both=list3.values(), user1=profile1, user2=profile2)
--- steam_api.py
+++ steam_api.py
@@ -23,7 +23,16 @@ def get_owned_games(userid):
         return res['games']
     raise ValueError('GetOwnedGames status response: ' + res.status_code)
 
+def get_player_summaries(user1, user2):
+    req = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/',
+                       {'key': STEAM_WEB_API_KEY, 'steamids': '{},{}'.format(user1, user2)})
+    if req.status_code == 200:
+        res = req.json()['response']
+        return (res['players'][0], res['players'][1]) if res['players'][0]['steamid'] == user1 else (res['players'][1], res['players'][0])
+    raise ValueError('GetOwnedGames status response: ' + res.status_code)
+
 
 # http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=XXXXXXXXXXXXXXXXXXXXXXX&vanityurl=userVanityUrlName
 # http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=XXXXXXXXXXXXX&include_appinfo=true&format=json
+# http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=XXXXXXXXXXXXXXXXXXXXXXX&steamids=XXXXXXX
 # http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{hash}.jpg

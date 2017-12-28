448d218c2bd007592a8cc7f27f54b8d41db37dd7
Alex <aamlima123@gmail.com>
2017-12-28 19:41:17 -0200

    Added checkUser route

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from steam_api import get_steamid, get_owned_games, get_player_summaries
+from flask import Flask, render_template, jsonify
+from steam_api import get_steamid, get_owned_games, get_player_summaries, get_player_summary
 app = Flask(__name__)
 
 
@@ -8,7 +8,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/<user1>/<user2>/')
+@app.route('/compare/<user1>/<user2>/')
 def compare_users(user1, user2):
     errors = []
     try:
@@ -56,3 +56,21 @@ def compare_users(user1, user2):
     games_only_u2 = games2 - games_both
 
     return render_template('compare.html', games_user1=games_only_u1, games_user2=games_only_u2, games_both=games_both, user1=profile1, user2=profile2)
+
+
+@app.route('/checkUser/<user>/')
+def check_user(user):
+    try:
+        userid = int(user)
+    except ValueError:
+        try:
+            userid = get_steamid(user)
+        except ValueError:
+            return jsonify(error='Custom url not found.')
+
+    try:
+        profile = get_player_summary(userid)
+    except ValueError:
+        return jsonify(error='Profile not found.')
+
+    return jsonify(profile)
--- steam_api.py
+++ steam_api.py
@@ -53,10 +53,22 @@ def get_player_summaries(user1, user2):
     if req.status_code == 200:
         res = req.json()['response']
         if len(res['players']) is 1:
-            raise ValueError('A profile(id={}) wasn\'t found.'.format(user1 if res['players'][0]['steamid'] == str(user1) else user2))
+            raise ValueError('A profile(id={}) wasn\'t found.'.format(
+                user1 if res['players'][0]['steamid'] == str(user1) else user2))
         elif len(res['players']) is 0:
             raise ValueError('No profiles found.')
         return (res['players'][0], res['players'][1]) if res['players'][0]['steamid'] == str(user1) else (res['players'][1], res['players'][0])
+    raise ValueError('GetPlayerSummaries status response: ' + res.status_code)
+
+
+def get_player_summary(user):
+    req = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/',
+                       {'key': STEAM_WEB_API_KEY, 'steamids': user})
+    if req.status_code == 200:
+        res = req.json()['response']
+        if len(res['players']) is 0:
+            raise ValueError('Profile not found.')
+        return res['players'][0]
     raise ValueError('GetPlayerSummaries status response: ' + res.status_code)
 
 
